385a6784bb94400a2f8d7fd24e1418fd4b9f7ee9
Pavel Kurkutov <kurkutovpa@gmail.com>
2023-10-11 22:38:44 +0700

    Add option description

--- src/CombatDicesTeam.Dialogues/DialogueOption.cs
+++ src/CombatDicesTeam.Dialogues/DialogueOption.cs
@@ -6,9 +6,11 @@ public sealed class DialogueOption<TParagraphConditionContext, TAftermathContext
     {
         TextSid = textSid;
         Next = nextNode;
+        Aftermath = new NullDialogueOptionAftermath<TAftermathContext>();
     }
 
-    public IDialogueOptionAftermath<TAftermathContext>? Aftermath { get; init; }
+    public IDialogueOptionAftermath<TAftermathContext> Aftermath { get; init; }
     public DialogueNode<TParagraphConditionContext, TAftermathContext> Next { get; }
     public string TextSid { get; }
+    public string? DescriptionSid { get; init; }
 }
---
+++ src/CombatDicesTeam.Dialogues/NullDialogueOptionAftermath.cs
@@ -0,0 +1,16 @@
+﻿namespace CombatDicesTeam.Dialogues;
+
+public sealed class NullDialogueOptionAftermath<TAftermathContext> : IDialogueOptionAftermath<TAftermathContext>
+{
+    public bool IsHidden => true;
+
+    public void Apply(TAftermathContext aftermathContext)
+    {
+        // Do nothing
+    }
+
+    public string GetDescription(TAftermathContext aftermathContext)
+    {
+        return string.Empty;
+    }
+}
